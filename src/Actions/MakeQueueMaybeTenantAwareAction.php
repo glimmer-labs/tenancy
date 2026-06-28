6bfa5b73d7da39db0d6bc7152a1cf3a8562530c0
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2026-06-28 16:23:23 -0600

    Fix is MaybeTenantAware job detection when `queues_are_tenant_aware_by_default` is disabled

--- src/Actions/MakeQueueMaybeTenantAwareAction.php
+++ src/Actions/MakeQueueMaybeTenantAwareAction.php
@@ -12,6 +12,8 @@ use ReflectionException;
 use Spatie\Multitenancy\Actions\MakeQueueTenantAwareAction;
 use Spatie\Multitenancy\Contracts\IsTenant;
 use Spatie\Multitenancy\Exceptions\CurrentTenantCouldNotBeDeterminedInTenantAwareJob;
+use Spatie\Multitenancy\Jobs\NotTenantAware;
+use Spatie\Multitenancy\Jobs\TenantAware;
 use Throwable;
 
 class MakeQueueMaybeTenantAwareAction extends MakeQueueTenantAwareAction
@@ -22,19 +24,15 @@ class MakeQueueMaybeTenantAwareAction extends MakeQueueTenantAwareAction
      */
     protected function bindOrForgetCurrentTenant(JobProcessing|JobRetryRequested $event): void
     {
-        if ($this->isTenantAware($event)) {
+        $reflection = $this->getJobReflection($event);
+
+        if ($this->jobIsTenantAware($reflection) || $this->jobIsMaybeTenantAware($reflection)) {
             try {
                 $this->bindAsCurrentTenant($this->findTenant($event)->makeCurrent());
 
                 return;
             } catch (CurrentTenantCouldNotBeDeterminedInTenantAwareJob $e) {
-                $reflection = $this->getJobReflection($event);
-
-                if (
-                    ! $reflection->implementsInterface(config('multitenancy.maybe_tenant_aware_interface',
-                        MaybeTenantAware::class)) &&
-                    ! in_array($reflection->name, config('multitenancy.maybe_tenant_aware_jobs'))
-                ) {
+                if (! $this->jobIsMaybeTenantAware($reflection)) {
                     $event->job->delete();
                     throw $e;
                 }
@@ -42,6 +40,36 @@ class MakeQueueMaybeTenantAwareAction extends MakeQueueTenantAwareAction
         }
 
         app(IsTenant::class)::forgetCurrent();
+    }
+
+    protected function jobIsTenantAware(ReflectionClass $reflection): bool
+    {
+        if ($reflection->implementsInterface(config('multitenancy.tenant_aware_interface', TenantAware::class))) {
+            return true;
+        }
+
+        if ($reflection->implementsInterface(config('multitenancy.not_tenant_aware_interface',
+            NotTenantAware::class))) {
+            return false;
+        }
+
+        if (in_array($reflection->name, config('multitenancy.tenant_aware_jobs'))) {
+            return true;
+        }
+
+        if (in_array($reflection->name, config('multitenancy.not_tenant_aware_jobs'))) {
+            return false;
+        }
+
+        return config('multitenancy.queues_are_tenant_aware_by_default') === true;
+    }
+
+    protected function jobIsMaybeTenantAware(ReflectionClass $reflection): bool
+    {
+        $concern = config('multitenancy.maybe_tenant_aware_interface', MaybeTenantAware::class);
+        $config = config('multitenancy.maybe_tenant_aware_jobs');
+
+        return $reflection->implementsInterface($concern) || in_array($reflection->name, $config);
     }
 
     /**
@@ -55,18 +83,10 @@ class MakeQueueMaybeTenantAwareAction extends MakeQueueTenantAwareAction
         $tenantId = Context::get($this->currentTenantContextKey());
 
         if (! $tenantId) {
-            if ($event instanceof JobProcessing) {
-                $event->job->delete();
-            }
-
             throw CurrentTenantCouldNotBeDeterminedInTenantAwareJob::noIdSet($event);
         }
 
         if (! $tenant = app(IsTenant::class)::find($tenantId)) {
-            if ($event instanceof JobProcessing) {
-                $event->job->delete();
-            }
-
             throw CurrentTenantCouldNotBeDeterminedInTenantAwareJob::noTenantFound($event);
         }
 
